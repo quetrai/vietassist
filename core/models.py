e1b3bf5cee405d5c5bca57c72b6637ec9394dd18
quetrai <traique006@gmail.com>
2026-08-12 12:46:52 +0700

    Add files via upload

--- core/models.py
+++ core/models.py
@@ -23,12 +23,7 @@ class User:
     role: Role
     active: bool = True
     rag_enabled: bool = True
-    paired: bool = False
 
     @property
     def can_use_group_summary(self) -> bool:
-        return self.paired and self.role in {Role.ROOT, Role.ZALO_ADMIN}
-
-    @property
-    def is_bot_user(self) -> bool:
-        return self.channel == Channel.TELEGRAM or self.paired
+        return self.role in {Role.ROOT, Role.ZALO_ADMIN}
